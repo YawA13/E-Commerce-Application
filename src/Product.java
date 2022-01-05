c6575bc76d11dda5973c38cdb56b3ae1fb6d672b
Yaw <yawasamoah13@hotmail.com>
2022-01-04 19:05:37 -0500

    Implemented Product class

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,26 @@
 public class Product {
+    private final String name;
+    private double price;
+    private String img; //potential change
+
+
+    public Product(String name, double price, String img) {
+        this.name = name;
+        this.price = price;
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+
 }
