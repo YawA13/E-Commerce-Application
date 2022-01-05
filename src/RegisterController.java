9336eae9e1eae30ceda71cc2cbccf8df636e8193
Yaw <yawasamoah13@hotmail.com>
2022-01-05 18:13:11 -0500

    implement RegisterController

--- src/RegisterController.java
+++ src/RegisterController.java
@@ -1,2 +1,35 @@
-public class RegisterController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RegisterController implements ActionListener {
+
+    private Register model;
+    private RegisterView view;
+
+    public RegisterController(Register model, RegisterView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        String firstName = view.getFirstName();
+        String lastName = view.getLastName();
+        String username = view.getUsername();
+        String password = view.getPassword();
+
+        model.setFirstName(firstName);
+        model.setLastName(lastName);
+        model.setUsername(username);
+        model.setPassword(password);
+        model.createNewAccount();
+    }
+
+    public void loginSuccessful()
+    {
+        view.dispose();
+        int customerId = model.getCustomerId();
+        new StoreController(customerId);
+    }
 }
