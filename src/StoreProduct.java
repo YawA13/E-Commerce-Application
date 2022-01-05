6314ce9d3e29bfa1935c2f6558d097f304642dba
Yaw <yawasamoah13@hotmail.com>
2022-01-04 19:06:24 -0500

    Implemented StoreProduct class

--- src/StoreProduct.java
+++ src/StoreProduct.java
@@ -1,2 +1,40 @@
 public class StoreProduct extends Product{
+
+    private final int id;
+    private int stock;
+
+    public StoreProduct(String name, double price, String img, int id, int stock)
+    {
+        super(name, price, img);
+        this.id = id;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock)
+    {
+        this.stock = stock;
+    }
+
+    public void addStock(int amountToAdd)
+    {
+        this.stock += amountToAdd;
+    }
+
+    public void removeStock(int amountToRemove)
+    {
+        this.stock -= amountToRemove;
+    }
+
+    public double getTotalAmount()
+    {
+        return getPrice()*stock;
+    }
 }
