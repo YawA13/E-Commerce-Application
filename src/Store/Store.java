4c7754db50d876b06afb0c090fa3105f04789db3
Yaw <yawasamoah13@hotmail.com>
2022-01-07 18:21:48 -0500

    create customer view and inventory fields

--- src/Store/Store.java
+++ src/Store/Store.java
@@ -1,4 +1,38 @@
 package Store;
 
+import Registration.RegistrationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Store {
+
+    private Customer customer;
+    private List<StoreView> views;
+    private ProductStockCollection inventory;
+
+    public Store()
+    {
+        customer = null;
+        views = new ArrayList<>();
+        inventory = new ProductStockMap();
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void addView(StoreView newView) {
+        views.add(newView);
+    }
+
+    public void removeView(StoreView oldView) {
+        for (int i = 0; i < views.size(); i++) {
+            if (views.get(i).equals(oldView)) {
+                views.remove(i);
+                break;
+            }
+        }
+    }
+
 }
