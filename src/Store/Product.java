79a2f17482471d152cf77858407902cd28bd63ab
Yaw <yawasamoah13@hotmail.com>
2022-01-07 18:03:57 -0500

    change img field name to imgUrl

--- src/Store/Product.java
+++ src/Store/Product.java
@@ -4,14 +4,14 @@ public class Product {
     private final int id;
     private final String name;
     private double price;
-    private String img; //potential change
+    private String imgUrl; //potential change
 
 
     public Product(int id, String name, double price, String img) {
         this.id = id;
         this.name = name;
         this.price = price;
-        this.img = img;
+        this.imgUrl = img;
     }
 
     public int getId() {
@@ -26,8 +26,8 @@ public class Product {
         return price;
     }
 
-    public String getImg() {
-        return img;
+    public String getImgUrl() {
+        return imgUrl;
     }
 
     @Override
