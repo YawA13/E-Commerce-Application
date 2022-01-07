ab5112266ae991c40bea2cb858d327fbe9a7f272
Yaw <yawasamoah13@hotmail.com>
2022-01-06 19:17:12 -0500

    add id as field for product and implemented an equals method

--- src/Store/Product.java
+++ src/Store/Product.java
@@ -1,15 +1,21 @@
 package Store;
 
 public class Product {
+    private final int id;
     private final String name;
     private double price;
     private String img; //potential change
 
 
-    public Product(String name, double price, String img) {
+    public Product(int id, String name, double price, String img) {
+        this.id = id;
         this.name = name;
         this.price = price;
         this.img = img;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -22,6 +28,22 @@ public class Product {
 
     public String getImg() {
         return img;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Product))
+        {
+            return false;
+        }
+        else if (obj == this)
+        {
+            return true;
+        }
+        else
+        {
+           return  ((Product) obj).id == this.id;
+        }
     }
 
 
