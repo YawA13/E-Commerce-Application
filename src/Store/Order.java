fe3e766de00a4c2b5d14a9ba25ab089f17f968b2
Yaw <yawasamoah13@hotmail.com>
2022-01-09 19:53:38 -0500

    Create method to save order in a database

--- src/Store/Order.java
+++ src/Store/Order.java
@@ -1,22 +1,48 @@
 package Store;
 
-import java.sql.Date;
+import General.DatabaseConnection;
+
+import java.sql.*;
 
 public class Order {
 
     private int customerId;
     private Date date;
     private ProductStockCollection cart;
+    private ProductStockCollection inventory;
 
     public Order(int id, ProductStockCollection cart)
     {
         customerId = id;
         date = null;
         this.cart = cart;
+        inventory = new ProductStockMap();
     }
 
     public void setCurrentDate() {
         java.util.Date utilDate=new java.util.Date();
         date=new java.sql.Date(utilDate.getTime());
+    }
+
+    public boolean saveOrderToDb() throws SQLException {
+        Connection connection = DatabaseConnection.getConnection();
+        Savepoint savepoint1 = null;
+
+        try
+        {
+            connection.setAutoCommit(false);
+            savepoint1 = connection.setSavepoint("Savepoint1");
+
+            return true;
+
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+            connection.rollback(savepoint1);
+            DatabaseConnection.closeConnection();
+            return false;
+        }
+
     }
 }
