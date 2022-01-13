bfe10c17edac1e7f124995fc15cbbf35dbe7561f
Yaw <yawasamoah13@hotmail.com>
2022-01-13 18:23:00 -0500

    javadoc comments for DatabaseConnection

--- src/General/DatabaseConnection.java
+++ src/General/DatabaseConnection.java
@@ -1,17 +1,27 @@
 package General;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+/**
+ * Singleton Class to retrive the database connection based off an external config file
+ */
 public class DatabaseConnection {
 
+    /**
+     * Connection to database
+     */
     private static Connection connection = null;
 
+    /**
+     * sets connection to MYSQL database using properites in config file and then return the connection
+     *
+     * @return          Connection, a specific connection to the MYSQL database
+     */
     private static Connection createConnection ()
     {
         Properties prop = new Properties();
@@ -31,6 +41,11 @@ public class DatabaseConnection {
         return connection;
     }
 
+    /**
+     * When connection is null, connection is set by createConnection. Gets the connection
+     *
+     * @return          Connection, the connection to MYSQL database
+     */
     public static Connection getConnection()
     {
         if (connection == null)
@@ -38,6 +53,9 @@ public class DatabaseConnection {
         return connection;
     }
 
+    /**
+     * Closes the connection and sets it to null to help identify the connection is closed.
+     */
     public static void closeConnection()
     {
         try
