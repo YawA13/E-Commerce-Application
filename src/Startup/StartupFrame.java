b439034d4b9431ed95284e4d2e8165186714463d
Yaw <yawasamoah13@hotmail.com>
2022-01-14 01:05:39 -0500

    comments for constructor of StartupFrame

--- src/Startup/StartupFrame.java
+++ src/Startup/StartupFrame.java
@@ -26,32 +26,38 @@ public class StartupFrame extends JFrame implements  StartupView{
         controller = new StartupController(model,this);
         model.addView(this);
 
+        //Outer panel directly connected to the frame to display colour
         JPanel colorPanel = new JPanel(new GridBagLayout());
         colorPanel.setBackground(new Color(0,103,103));
 
+        //panel to add the main buttons
         JPanel mainPanel = new JPanel();
         mainPanel.setBackground(new Color(0,0,0,0));
         mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
 
+        //set register button
         JButton registerBtn = new JButton("Register");
         registerBtn.setActionCommand("register");
         registerBtn.addActionListener(controller);
         registerBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        //set the login button
         JButton loginBtn = new JButton("Login");
         loginBtn.setActionCommand("login");
         loginBtn.addActionListener(controller);
         loginBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        //adds all components to frame
+        //adds all components to main panel
         mainPanel.add(registerBtn);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
         mainPanel.add(loginBtn);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
+
         colorPanel.add(mainPanel);
         this.add(colorPanel);
 
+        //set common properties for the frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(600,300);
         this.setPreferredSize(new Dimension(600,300));
