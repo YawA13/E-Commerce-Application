f63f704c7f62988b4ea32e67bc3a5da3ac1fd02f
Yaw <yawasamoah13@hotmail.com>
2022-01-14 01:06:00 -0500

    comments for constructor of RegistrationFrame

--- src/Registration/RegistrationFrame.java
+++ src/Registration/RegistrationFrame.java
@@ -30,49 +30,58 @@ public class RegistrationFrame extends JFrame implements RegistrationView {
         model.addView(this);
         controller = new RegistrationController(model,this);
 
+        //Outer panel directly connected to the frame to display colour
         JPanel colorPanel = new JPanel(new GridBagLayout());
         colorPanel.setBackground(new Color(0,103,103));
 
+        //main panel for all text field and buttons to be added
         JPanel mainPanel = new JPanel();
         mainPanel.setBackground(new Color(0,0,0,0));
         mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
 
-        //create label and field for textbox
+        //create label and text field for first name
         JLabel firstNameLabel = new JLabel("First Name");
         firstNameLabel.setForeground(Color.WHITE);
         firstNameTextBox = new JTextField();
 
+        //create label and text field for last name
         JLabel lastNameLabel = new JLabel("Last Name");
         lastNameLabel.setForeground(Color.WHITE);
         lastNameTextBox = new JTextField();
 
+        //create label and text field for username
         JLabel usernameLabel = new JLabel("Username");
         usernameLabel.setForeground(Color.WHITE);
         usernameTextBox = new JTextField();
 
+        //create label and text field for password
         JLabel passwordLabel = new JLabel("Password");
         passwordLabel.setForeground(Color.WHITE);
         passwordTextBox = new JTextField();
 
+
         JButton registrationBtn = new JButton("Registration");
         registrationBtn.addActionListener(controller);
 
-        //adds all components to frame
+        //adds label, then empty space, then text field, then another empty space component for first name to main panel
         mainPanel.add(firstNameLabel);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
         mainPanel.add(firstNameTextBox);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
+        //adds label, then empty space, then text field, then another empty space component for last name to main panel
         mainPanel.add(lastNameLabel);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
         mainPanel.add(lastNameTextBox);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
+        //adds label, then empty space, then text field, then another empty space component for username to main panel
         mainPanel.add(usernameLabel);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
         mainPanel.add(usernameTextBox);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
+        //adds label, then empty space, then text field, then another empty space component for password to main panel
         mainPanel.add(passwordLabel);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
         mainPanel.add(passwordTextBox);
@@ -82,6 +91,7 @@ public class RegistrationFrame extends JFrame implements RegistrationView {
         colorPanel.add(mainPanel);
         this.add(colorPanel);
 
+        //set common properties for the frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(600,300);
         this.setPreferredSize(new Dimension(600,300));
